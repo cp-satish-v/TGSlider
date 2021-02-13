de3c97bba5e53eb40c0baef6e400328f36a7ddc6
satish.v <satish.v@canopas.com>
2021-02-13 20:59:12 +0530

    Initial Commit

---
+++ TGSlider/TGSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TGSliderApp.swift
+//  TGSlider
+//
+//  Created by Divyesh Vekariya on 13/02/21.
+//
+
+import SwiftUI
+
+@main
+struct TGSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
