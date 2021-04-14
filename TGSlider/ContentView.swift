2e2b52c063217e00ece408c6e9a52a14aeb72a58
cp-amisha-i <amisha.i@canopas.com>
2021-04-14 16:32:52 +0530

    set progress text label

--- TGSlider/ContentView.swift
+++ TGSlider/ContentView.swift
@@ -10,39 +10,52 @@ import SwiftUI
 struct ContentView: View {
     
     @Binding var percentage: Float // or some value binded
-    @State var segmentWidth:CGFloat = 3
-    @State var segmentHeight:CGFloat = 16
+    @State var segmentWidth: CGFloat = 3
+    @State var segmentHeight: CGFloat = 35
+    @State var smallSegmentHeight: CGFloat = 0.5
+    @State var totalSegment: Int = 10
+    @State var progressType: String = "°"
+    @State var fontSize: CGFloat = 15
 
     var body: some View {
         GeometryReader { geometry in
             // TODO: - there might be a need for horizontal and vertical alignments
             ZStack(alignment: .leading) {
                 HStack(alignment: .center) {
-                    ForEach(Range<Int>(0...8)) { _ in
+                    ForEach(Range<Int>(0...totalSegment - 1)) { _ in
                         Spacer(minLength: 3)
                         Rectangle()
-                            .frame(width:segmentWidth, height: min(segmentHeight * 0.6, geometry.size.height))
+                            .frame(width: segmentWidth, height: min(segmentHeight * smallSegmentHeight, geometry.size.height))
                             .foregroundColor(.gray)
+                            .cornerRadius(4)
                         Spacer(minLength: 3)
                     }
-                    
                 }
                 Rectangle()
                     .foregroundColor(.gray)
                     .frame(height: 3)
+                    .cornerRadius(7.5)
+                
+                Text("\(Int(ceil(self.percentage)))\(progressType)")
+                    .font(.system(size: fontSize))
+                    .frame(alignment: .center)
+                    .padding(.bottom, 60)
+                    .padding(.leading, -6)
+                    .offset(CGSize(width: max(min(((geometry.size.width - segmentWidth) * CGFloat(self.percentage / 100)), geometry.size.width - 26), 6), height: 0))
+                
                 Rectangle()
                     .frame(width:segmentWidth, height: min(segmentHeight, geometry.size.height))
                     .foregroundColor(.accentColor)
+                    .cornerRadius(4)
                     .offset(CGSize(width: (geometry.size.width - segmentWidth) * CGFloat(self.percentage / 100), height: 0))
             }
             .padding([.top, .bottom], /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-            .cornerRadius(12)
             .gesture(DragGesture(minimumDistance: 0)
                 .onChanged({ value in
                     // TODO: - maybe use other logic here
                     self.percentage = min(max(0, Float(value.location.x / geometry.size.width * 100)), 100)
                 }))
-        }.frame(height: 36)
+        }.frame(height: 80)
     }
 }
 
